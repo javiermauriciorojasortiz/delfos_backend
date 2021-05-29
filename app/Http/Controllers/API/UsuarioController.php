9247b21e7f34c4f486b7ba40c89955c1e6b95466
Cristian Rios <riospcristian@hotmail.com>
2021-05-28 23:39:43 -0500

    Api inicial para login

--- app/Http/Controllers/API/UsuarioController.php
+++ app/Http/Controllers/API/UsuarioController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Seguridad\UsuarioModel;
+use PhpParser\Node\Expr\Empty_;
 
 class UsuarioController extends Controller
 {
@@ -35,9 +36,23 @@ class UsuarioController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($id, $pass)
     {
-        //
+        $usuario = UsuarioModel::where('usr_identificacion', '=', $id)
+            ->where('usr_clave', '=', $pass)
+            ->get();
+
+        if (count($usuario) > 0) {
+            return response()->json([
+                'res' => true,
+                'usuario' => $usuario,
+            ]);
+        } else {
+            return response()->json([
+                'res' => false,
+                'usuario' => $usuario,
+            ]);
+        }
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('usuarios', [UsuarioController::class, 'index']);
+Route::get('login/{id}/{pass}', [UsuarioController::class, 'show']);
